6f7f1d498b287bf7fe2b433c17fce0af842a2ed4
GURJOTEXPERT <91006342+GURJOTEXPERT@users.noreply.github.com>
2021-09-19 14:11:15 +0530

    Add files via upload

---
+++ WebsiteScanner.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+apt install figlet
+apt install dnsutils
+apt install cowsay
+clear
+sleep 3
+figlet Website Scanner
+sleep 1
+
+echo -n  "ENTER THE DOMAIN :"
+read a
+sleep 3
+echo -n  "ENTER THE FILE NAME TO SAVE THE SCANNING :"
+read b
+sleep 3
+apt install nmap
+sleep 3
+nmap $a -oN $b.txt
+sleep 3
+nslookup $a
+sleep 7
+echo "____________"
+echo "PLEASE WAIT "
+echo "____________"
+sleep 2
+clear
+cowsay "THANK FOR USING"
+figlet Website Scanner
